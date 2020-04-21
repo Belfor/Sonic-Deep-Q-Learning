94d266b2435c0db936addb7e36ea8de724499979
Belfor <alemandeleon86@gmail.com>
2020-04-21 21:57:52 +0100

    rainbow dqn implemented

--- SonicAgent.py
+++ SonicAgent.py
@@ -5,12 +5,14 @@ import numpy as np
 
 import json
 import random 
+import math
 import keras
 from pathlib import Path
 from DQN import DQN
 from collections import deque
 from keras import backend as k
 import tensorflow as tf
+from PriorityExperienceMemory import PriorityExperienceMemory
 
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -34,12 +36,24 @@ class SonicAgent():
        
         self.lr =  parameter["agent"]["learning_rate"]  
         self.gamma = parameter["agent"]["gamma"]  
-        self.memory = deque(maxlen = parameter["agent"]["max_memory"] )
+        self.memory = PriorityExperienceMemory(parameter["agent"]["max_memory"])
+      
         self.batch_size = parameter["agent"]["batch_size"]  
                   
         self.training = training
         self.path_model=parameter["agent"]["models"] 
         
+        # Number of future states used to form memories in n-step dqn 
+        self.n_step = parameter["agent"]["n_step"] 
+
+        #The support for the value distribution. Set to 51 for C51
+        self.num_atoms = 51
+        
+        # Break the range of rewards into 51 uniformly spaced values (support)
+        self.v_max = 5 * self.n_step # Rewards are clipped to -20, 20
+        self.v_min = -5 * self.n_step
+        self.delta_z = (self.v_max - self.v_min) / float(self.num_atoms - 1)
+        self.z = [self.v_min + i * self.delta_z for i in range(self.num_atoms)]
         
     def createModel(self,env,file_name_h5 = None):
         self.action_space = env.action_space    
@@ -47,9 +61,8 @@ class SonicAgent():
 
         dqn = DQN(self.observation_shape, self.action_space.n, self.lr)
            
-        self.model = dqn.createModel()
-        self.target_model = dqn.createModel()
-      
+        self.model = dqn.rainbowDqn()
+        self.target_model = dqn.rainbowDqn()  
        
         if file_name_h5 != None:
             file = Path(self.path_model + file_name_h5)
@@ -61,47 +74,103 @@ class SonicAgent():
     def update_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
         
-    def policy(self, obs, epsilon = None):
+    def get_action(self, obs):
         obs = obs[np.newaxis,:]
-      
-        if np.random.random() < epsilon and self.training:
-            action = random.choice([a for a in range(self.action_space.n)])
-        else:
-            action = np.argmax(self.model.predict(obs))
+        action_distributions = self.model.predict(obs)
+
+        weighted_dists = np.multiply(np.vstack(action_distributions), np.array(self.z))
+        
+        # Sum the weighted values from each distribution and find the one with 
+        # the largest expected value.
+        avg_dist_values = np.sum(weighted_dists, axis=1)
+        print(avg_dist_values);
+        action = np.argmax(avg_dist_values)
             
         return action
     
     def save_memory(self, obs, action, reward, next_obs, done):
-        self.memory.append((obs, action, reward, next_obs, done))
+        _, _, error = self.target_rewards([(obs, action, reward, next_obs, done)])
+        self.memory.add((obs, action, reward, next_obs, done), error[0])
        
+    def update_memory(self, size, indicies,error):
+        for i in range(size):
+            idx = indicies[i]
+            self.memory.update(idx, error[i])
+    
     def replay_and_learn(self):
        
-        mini_batch = random.sample(self.memory, self.batch_size)
-        inputs = np.zeros(((self.batch_size,) + self.observation_shape))
-        targets = np.zeros((self.batch_size, self.action_space.n))
-        
-        for i in range (self.batch_size):
-            obs, action, reward, next_obs, done = mini_batch[i]
-            targets[i,action] = self.target_reward(obs, action, reward, next_obs, done)
-            inputs[i] = obs[np.newaxis,:]
+        mini_batch, indicies = self.memory.sample(self.batch_size)
+     
+        inputs, targets, error = self.target_rewards(mini_batch)
+        self.update_memory(self.batch_size,indicies,error)  
         self.model.train_on_batch(inputs,targets)
             
             
-    def target_reward(self, obs, action, reward, next_obs, done):
-       
-        obs = obs[np.newaxis,:]
-        next_obs = next_obs[np.newaxis,:]
-       
-        if done:
-            return reward 
-        else:
-            return reward + self.gamma * np.amax(self.target_model.predict(next_obs)[0])
-       
+    def target_rewards(self, batch):
+        batch_size = len(batch)
+        inputs =  np.zeros(((batch_size,) + self.observation_shape)) 
+        targets = np.zeros(((batch_size,) + self.observation_shape)) 
+        actions, rewards, dones = [], [], []
+        
+        for i in range(batch_size):
+            obs, action, reward, next_obs, done = batch[i]
+            inputs[i] = obs[np.newaxis,:]
+            actions.append(action)
+            rewards.append(reward)
+            targets[i] = next_obs[np.newaxis,:]
+            dones.append(done)
+
+        m_prob = [np.zeros((batch_size, self.num_atoms)) for i in range(self.action_space.n)]
+
+        model_dist = self.model.predict(targets)
+        target_dist = self.target_model.predict(targets)
+
+        # Stack all of the distributions from all actions and all inputs 
+        # (batch_size * action_size, num_atoms)
+        stacked_dist = np.vstack(model_dist)
+
+        # Multiply all distributions by value ranges, then sum each distribution,
+        # leaving a (batch_size, num_actions) matrix for each input in the batch.
+        q_values = np.sum(np.multiply(stacked_dist, np.array(self.z)), axis = 1)
+        q_values = q_values.reshape((len(batch), self.action_space.n), order='F')
+
+        next_act_idx = np.argmax(q_values, axis=1)
+
+        error = []
+
+        for i in range(batch_size):
+            prev_q = np.sum(model_dist[actions[i]][i])
+
+            if dones[i]:        
+                Tz = min(self.v_max, max(self.v_min, rewards[i]))
+                # Determine which segments the reward belongs to
+                bj = math.floor((Tz - self.v_min) / self.delta_z) 
+                segment_l = math.floor(bj)
+                segment_u = math.ceil(bj)
+                # Convert the rough segment value to indicies for upper and lower.
+                # Add the portion of the range (0-1) which belongs to each segment. 
+                m_prob[actions[i]][i][int(segment_l)] += (segment_u - bj)
+                m_prob[actions[i]][i][int(segment_u)] += (bj - segment_l)
+            else:
+                for atom in range(self.num_atoms):
+                    # Ensure rewards are clipped.
+                    Tz = min(self.v_max, max(self.v_min, rewards[i] + self.gamma * self.z[atom])) 
+                    # Determine which segments the reward belongs to
+                    bj = math.floor((Tz - self.v_min) / self.delta_z) 
+                    segment_l = math.floor(bj)
+                    segment_u = math.ceil(bj)
+                    lower_prob = target_dist[next_act_idx[i]][i][atom] * (segment_u - bj)
+                    upper_prob = target_dist[next_act_idx[i]][i][atom] * (bj - segment_l)
+                    m_prob[actions[i]][i][int(segment_l)] += lower_prob
+                    m_prob[actions[i]][i][int(segment_u)] += upper_prob
+
+            q_update = np.sum(m_prob[actions[i]][i])            
+            error.append(abs(prev_q - q_update))
+        
+        return inputs, m_prob, error
               
     def save_model(self,filename):
         self.model.save_weights(filename)
-
-     
         
     def load_model(self,filename):
         self.model.load_weights(filename)
--- train.py
+++ train.py
@@ -5,14 +5,15 @@ Created on Mon Apr 13 10:02:19 2020
 
 @author: Belfor
 """
-
+import csv
 import json
 import numpy as np
+from collections import deque
 from SonicAgent import SonicAgent
 from utils.utils import make_env
 from utils.levelManager import LevelManager
 from retro_contest.local import make
-from LinearDecaySchedule import LinearDecaySchedule
+
 
 from tensorboardX import SummaryWriter
 
@@ -21,19 +22,25 @@ parameter = json.load(open('sonic.json', 'r'))
 agent = parameter["agent"]
 enviroment = parameter["enviroment"]
 
-steps_episode = agent["steps_episode"];
+steps_episode = agent["steps_episode"]
 writer = SummaryWriter(agent["logs"])
 update_target_freq = agent["update_target_freq"]
 timestep_per_train = agent["timestep_per_train"]
 max_num_episodes =agent["episodes"]
 
-def training(env,sonic,global_step_num,epsilon_decay):
+n_step = agent["n_step"]
+n_step_rewards = deque(maxlen=n_step) 
+n_step_exp = deque(maxlen=n_step)
+
+def training(env,sonic,global_step_num):
     
     total_reward = 0.0
     
     sonic.createModel(env)
      
     for episodes in range(max_num_episodes):
+        n_step_rewards.clear()
+        n_step_exp.clear()
         obs = env.reset()
         done = False
         total_reward = 0.0
@@ -41,12 +48,20 @@ def training(env,sonic,global_step_num,epsilon_decay):
         steps = 0
         print("Empieza Episodio #{}".format(episodes + 1))
         while not done:
-            action = sonic.policy(obs,epsilon_decay(global_step_num))
-            writer.add_scalar("epsilon", epsilon_decay(global_step_num),global_step_num)
-            
-            next_obs, reward, done, info = env.step(action)
-            
-            sonic.save_memory(obs,action,reward,next_obs,done)
+            action = sonic.get_action(obs)      
+            next_obs, reward, done, _ = env.step(action)
+
+            n_step_rewards.appendleft(reward)
+            n_step_exp.append((next_obs, action, done))
+
+            if (len(n_step_rewards) >= sonic.n_step):
+                total_n_reward = 0
+                for i in range(len(n_step_rewards)):
+                    total_n_reward += ((sonic.gamma ** i) * n_step_rewards[i])
+
+                obs, action, _ = n_step_exp[-1]
+                n_state_obs, _, n_state_done = n_step_exp[0]
+                sonic.save_memory(obs,action,total_n_reward,n_state_obs,n_state_done)
                         
             obs = next_obs
             total_reward += reward        
@@ -54,27 +69,23 @@ def training(env,sonic,global_step_num,epsilon_decay):
                  
             if enviroment["render"]:
                 env.render()
-                
-            if len(sonic.memory) > 2 * agent["batch_replay"]:
-                sonic.replay_and_learn()
+            
                 
             global_step_num += 1
             steps += 1
-            
-            if ((global_step_num % update_target_freq) == 0):
-                 sonic.update_target_model()
+            if (global_step_num > agent["max_memory"]):
+                if ((global_step_num % update_target_freq) == 0):
+                    sonic.update_target_model()
 
-            if ((global_step_num % timestep_per_train) == 0):
-                sonic.replay_and_learn()
-    
-        global_step_num += steps_episode - steps
+                if ((global_step_num % timestep_per_train) == 0):
+                    sonic.replay_and_learn()
         
+        if (global_step_num > agent["max_memory"]):
+            if ((episodes % 50) == 0):
+                sonic.update_target_model()
 
-        if ((episodes % 50) == 0):
-            sonic.update_target_model()
-
-        if ((episodes % 50) == 0):
-            sonic.replay_and_learn()
+            if ((episodes % 50) == 0):
+                sonic.replay_and_learn()
             
         if (total_reward > best_reward):
             best_reward = total_reward
@@ -82,10 +93,8 @@ def training(env,sonic,global_step_num,epsilon_decay):
    
        
         print("Episodio #{} finalizado con recompensa {}".format(episodes + 1, total_reward))
-        writer.add_scalar('ep_reward', total_reward, global_step_num)
-      
-      
-        
+        writer.add_scalar('rewards', total_reward, global_step_num)
+           
     env.close()
     sonic.save_model('models/sonic_model_final.h5')
     writer.close()
@@ -103,14 +112,8 @@ if __name__ == '__main__':
         levels = enviroment["selected_maps"]
     else:
         levels = [0..levelManager.size_maps_training() - 1]
-    
-    epsilon_initial = agent["epsilon_max"]
-    epsilon_final = agent["epsilon_min"]
- 
+
     setps_per_episode = agent["steps_episode"]
-    linear_schedule = LinearDecaySchedule( epsilon_initial,
-                                           epsilon_final, 
-                                           len(levels) * max_num_episodes * setps_per_episode)
     
     sonic = SonicAgent(True)
       
@@ -119,6 +122,6 @@ if __name__ == '__main__':
         level = levelManager.getMap(i)
         env = make(level[0],level[1])
         env = make_env(env,allow_backtracking=True)
-        training(env,sonic,global_step_num,linear_schedule)
+        training(env,sonic,global_step_num)
 
         print("Mapa #{} Finaliza...".format(i))
